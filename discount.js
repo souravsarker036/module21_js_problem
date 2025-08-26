324c86b4a266ad04b43e033e1807fe1c6da2a0c4
Sourav Sarker <souravsarker036@gmail.com>
2025-08-27 00:41:42 +0600

    layerd discount

---
+++ discount.js
@@ -0,0 +1,17 @@
+function discountPrice(quantity){
+    if (quantity <= 100){
+        const total = quantity * 100;
+        return total;
+    }
+    else if(quantity<=200){
+        const total = quantity * 90;
+        return total;
+    }
+    else{
+        const total = quantity * 70;
+        return total;
+    }
+}
+
+const total = discountPrice(101)
+console.log(total)
